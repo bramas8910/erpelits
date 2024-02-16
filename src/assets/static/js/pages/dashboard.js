9794725b027919816e592f84cc28fe50afa21811
bramas8910 <bramtok@gmail.com>
2024-02-16 08:21:50 +0700

    accounting done

--- src/assets/static/js/pages/dashboard.js
+++ src/assets/static/js/pages/dashboard.js
@@ -39,7 +39,7 @@ var optionsProfileVisit = {
 }
 let optionsVisitorsProfile = {
   series: [70, 30],
-  labels: ["Male", "Female"],
+  labels: ["PA", "HAR"],
   colors: ["#435ebe", "#55c6e8"],
   chart: {
     type: "donut",
